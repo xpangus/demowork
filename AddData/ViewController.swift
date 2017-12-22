6aeb66562fad3a95f1434696fe3ab96247de2a2b
Shr Ying Pan <apppeterpan@gmail.com>
2017-12-22 10:32:41 +0800

    Initial Commit

---
+++ AddData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddData
+//
+//  Created by SHIH-YING PAN on 22/12/2017.
+//  Copyright © 2017 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
